3ce8aa85c6db9f54935cb55d11629972bdc8ac26
alexbohdanov <83947461+alexbohdanov@users.noreply.github.com>
2021-08-26 22:54:19 +1000

    Add files via upload

---
+++ ДЗ_6_Работа с сетью/HomeWork6.java
@@ -0,0 +1,48 @@
+package ru.geekbrains.lesson6;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class HomeWork6 {
+
+    public static final String HOST = "dataservice.accuweather.com";
+    public static final String FORECAST_URL = "forecasts";
+    public static final String DAILY_URL = "daily";
+    public static final String FIVE_DAYS_URL = "5day";
+    public static final String API_VERSION = "v1";
+    public static final String CITY_ID = "295212";
+    public static final String API_KEY = "bISdX4VlvYXioMxSlivywiajXe3qLTms";
+    public static final String LANGUAGE = "ru-ru";
+    public static final String METRIC = "true";
+
+
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host(HOST)
+                .addPathSegment(FORECAST_URL)
+                .addPathSegment(API_VERSION)
+                .addPathSegment(DAILY_URL)
+                .addPathSegment(FIVE_DAYS_URL)
+                .addPathSegment(CITY_ID)
+                .addQueryParameter("apikey", API_KEY)
+                .addQueryParameter("language", LANGUAGE)
+                .addQueryParameter("metric", METRIC)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println(Objects.requireNonNull(response.body()).string());
+    }
+}
